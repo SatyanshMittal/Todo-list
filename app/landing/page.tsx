92fcaa155f195beeb6d9ef79152d07d6bbfbdaf0
Satyansh Mittal <mittalsatyansh@gmail.com>
2024-07-13 23:36:05 +0530

    shadcn added

---
+++ app/landing/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+
+export default function page() {
+  return (
+    <div className='h-screen w-full'>
+        I mean why not?
+        <Button variant="outline">Button</Button>
+
+    </div>
+  )
+}
